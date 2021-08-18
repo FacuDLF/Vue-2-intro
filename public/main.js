368071c7ce1c27355ced89098635c73cd7aeeb7e
FacuDLF <facundo.delafuente@radiumrocket.com>
2021-08-18 09:56:53 -0300

    Vue2-inventory

--- public/main.js
+++ public/main.js
@@ -2,6 +2,8 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        image: 'assets/greenSocks.jpg' 
+        image: 'assets/greenSocks.jpg',
+        inventory: 8,
+        /*inStock: 'true'*/
     }
 })
