e28214cf0987476c167464eaefdf19b8e71907f1
FacuDLF <facundo.delafuente@radiumrocket.com>
2021-08-18 10:18:31 -0300

    Vue2-addToCart

--- public/main.js
+++ public/main.js
@@ -4,6 +4,25 @@ var app = new Vue({
         product: 'Socks',
         image: 'assets/greenSocks.jpg',
         inventory: 8,
-        details: ["80% cotton", "20% polyester", "Gender neutral"]
+        details: ["80% cotton", "20% polyester", "Gender neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: 'assets/greenSocks.jpg'
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+                variantImage: 'assets/blueSocks.jpg'
+            }
+        ],
+        cart: 0 
+    },
+    methods: {
+        addToCart: function () {
+            this.cart += 1
+        }
     }
+
 })
